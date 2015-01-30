5f1fb5e46a3d0f546a6246f05c852be5c1d2b850
Rehan Sheikh <smrehan6@gmail.com>
2015-01-30 16:27:09 +0530

    initial commit

---
+++ WeatherForecast/src/me/smr/weatherforecast/App.java
@@ -0,0 +1,11 @@
+package me.smr.weatherforecast;
+
+import android.app.Application;
+
+public class App extends Application {
+	@Override
+	public void onCreate() {
+		// TODO implementation pending
+		super.onCreate();
+	}
+}
