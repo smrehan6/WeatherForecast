dee8c1072c951bdde8a6c9383a9b594369cb1cdb
Rehan Sheikh <smrehan6@gmail.com>
2021-11-25 12:37:04 +0530

    setup toolbar with navigation

--- app/src/main/java/me/smr/weatherforecast/HomeActivity.kt
+++ app/src/main/java/me/smr/weatherforecast/HomeActivity.kt
@@ -7,6 +7,9 @@ import android.util.Log
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupWithNavController
 import dagger.hilt.android.AndroidEntryPoint
 import me.smr.weatherforecast.viewmodels.HomeViewModel
 
@@ -24,6 +27,11 @@ class HomeActivity : AppCompatActivity() {
 
     private fun setupToolbar() {
         val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.nav_host_fragment) as NavHostFragment
+        val navController = navHostFragment.navController
+        val appBarConfiguration = AppBarConfiguration(navController.graph)
+        toolbar.setupWithNavController(navController, appBarConfiguration)
         setSupportActionBar(toolbar)
     }
 
